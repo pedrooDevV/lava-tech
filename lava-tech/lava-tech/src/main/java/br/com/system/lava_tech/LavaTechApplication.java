aa3e6576fea953b924614ee837440c5e7212373b
Pedro-Costa <106925828+pedrooDevV@users.noreply.github.com>
2025-09-30 22:47:11 -0300

    Add files via upload
    Nosso projeto.

---
+++ lava-tech/lava-tech/src/main/java/br/com/system/lava_tech/LavaTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.system.lava_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavaTechApplication.class, args);
+	}
+
+}
---
+++ lava-tech/lava-tech/src/test/java/br/com/system/lava_tech/LavaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.system.lava_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
